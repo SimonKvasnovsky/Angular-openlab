f830a675ebfe3d571c4c8e0bdeb35258a9864182
simon.kvasnovsky@gmail.com <simon.kvasnovsky@gmail.com>
2021-10-26 09:56:54 +0200

    Zoznam itemov(nedokonceny)

---
+++ angular-tour-of-heroes/src/app/mock-items.ts
@@ -0,0 +1,15 @@
+import { Item } from "./items"
+
+export const ITEMS: Item[] = [
+  { id: 11, name: 'Sword', price: 145 },
+  { id: 12, name: 'Bow', price: 150},
+  { id: 13, name: 'Arrow', price: 59},
+  { id: 14, name: 'Shield', price: 143 },
+  { id: 15, name: 'Potion', price: 25},
+  { id: 16, name: 'Bomb', price: 90},
+  { id: 17, name: 'Sunglasses', price: 30 },
+  { id: 18, name: 'Dagger', price: 70},
+  { id: 19, name: 'Axe', price: 120},
+  { id: 20, name: 'Armor', price: 200 }
+
+];
