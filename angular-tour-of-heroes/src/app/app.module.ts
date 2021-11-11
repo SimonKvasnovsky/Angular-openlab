4f6eedd2c1c1c13a5f3989963a3a3ec188521d05
simon.kvasnovsky@gmail.com <simon.kvasnovsky@gmail.com>
2021-11-11 09:14:01 +0100

    Extension (done)

--- angular-tour-of-heroes/src/app/app.module.ts
+++ angular-tour-of-heroes/src/app/app.module.ts
@@ -4,12 +4,16 @@ import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
 import { ItemsComponent } from './items/items.component'; // <-- NgModel lives here
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { ItemDetailComponent } from './item-detail/item-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
-    ItemsComponent
+    ItemsComponent,
+    HeroDetailComponent,
+    ItemDetailComponent
   ],
   imports: [
     BrowserModule,
