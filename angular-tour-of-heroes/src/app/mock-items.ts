2c087bd12bed9b61f5f7c4a00afe7a334b6f3354
simon.kvasnovsky@gmail.com <simon.kvasnovsky@gmail.com>
2021-11-24 22:31:47 +0100

    create  new mock-items

--- angular-tour-of-heroes/src/app/mock-items.ts
+++ angular-tour-of-heroes/src/app/mock-items.ts
@@ -12,4 +12,15 @@ export const ITEMS: Item[] = [
   { id: 9, name: 'Axe', price: 120},
   { id: 10, name: 'Armor', price: 200 },
 
+  { id: 11, name: 'Iron Sword', price: 145 },
+  { id: 12, name: 'Wooden Bow', price: 150},
+  { id: 13, name: 'Wooden Arrow', price: 59},
+  { id: 14, name: 'Iron Shield', price: 143 },
+  { id: 15, name: 'Heal Potion', price: 25},
+  { id: 16, name: 'Big Bomb', price: 90},
+  { id: 17, name: 'Good Sunglasses', price: 30 },
+  { id: 18, name: 'Silver Dagger', price: 70},
+  { id: 19, name: 'Iron Axe', price: 120},
+  { id: 20, name: 'Gold Armor', price: 200 },
+
 ];
